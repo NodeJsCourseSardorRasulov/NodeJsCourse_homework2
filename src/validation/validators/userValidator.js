260b212f454d8e65fbb1b9ac8ac0ff4ad6735474
Sardor Rasulov <sardor_rasulov@epam.com>
2023-01-25 23:28:54 +0500

    created validator middleware

--- src/validation/validators/userValidator.js
+++ src/validation/validators/userValidator.js
@@ -1,4 +1,14 @@
-import { validator } from "./validator.js";
 import { userSchema } from "../schemas/index.js";
 
-export const userMiddlewareValidator = validator.query(userSchema);
+export const userMiddlewareValidator = (req, res, next) => {
+  const { error } = userSchema.validate(req.body, {
+    aboartEarly: false,
+    allowUnknown: false
+  });
+
+  if (error?.isJoi) {
+    res.status(400).json(error.details);
+  } else {
+    next();
+  }
+};
