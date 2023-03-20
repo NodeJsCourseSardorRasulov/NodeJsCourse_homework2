4d12782ae2528711feadf475a5d2d25cb7aca5b7
Sardor Rasulov <sardor_rasulov@epam.com>
2023-03-20 23:06:59 +0500

    asyncHandler added for enabling error handling for error handling middleware

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,5 @@ export * from './getPermissionCode.js';
 export * from './attachServiceInfoToResponse.js';
 export * from './getServiceName.js';
 export * from './getFunctionName.js';
+export * from './asyncHandler.js';
+
