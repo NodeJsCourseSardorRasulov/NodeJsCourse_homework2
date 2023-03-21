d0a0c36cf8961b3032c70759d6d25c078a48a2b8
Sardor Rasulov <sardor_rasulov@epam.com>
2023-03-21 16:42:34 +0500

    rename morgan logger middleware, server.js file updated

--- src/server.js
+++ src/server.js
@@ -2,19 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import { serverConnectionConfig } from './configs/index.js';
-import { createLoggerMiddleware } from './logging/index.js';
+import { createMorganLoggerMiddleware } from './logging/index.js';
 
 const app = express();
-const loggerMiddleware = createLoggerMiddleware();
+const morganLoggerMiddleware = createMorganLoggerMiddleware();
 
 const { port } = serverConnectionConfig;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(loggerMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is launched on port ${port}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morganLoggerMiddleware);
 
 export default app;
